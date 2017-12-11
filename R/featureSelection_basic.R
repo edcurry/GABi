ee7f4ea3f34a7282426933bbf5168a36401443c6
Dan <danvjmusic@gmai.com>
2017-12-11 14:49:25 +0000

    minor code rearranging

--- R/featureSelection_basic.R
+++ R/featureSelection_basic.R
@@ -1,8 +1,10 @@
 featureSelection.basic <- function(cols){
-        features <- 0
-        if(length(cols)>1){
-                features <- diffRows[which(rowMeans(x[diffRows,cols])>fitnessArgs$consistency)]
-        }
-        features
+    features <- 0
+
+    if(length(cols) > 1){
+        features <- diffRows[which(rowMeans(x[diffRows, cols]) > fitnessArgs$consistency)]
+    }
+    
+    features
 }
 
